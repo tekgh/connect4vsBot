6193e3d8e85420d4511044a70692a813fc3432b7
tek <126421917+tekgh@users.noreply.github.com>
2025-12-02 20:30:01 +0100

    Endlessly playable version without win condition

---
+++ connect4.py
@@ -0,0 +1,29 @@
+import random
+board = [["0" for _ in range(7)] for _ in range(6)]
+def BoardPrint():
+    for row in range(5, -1, -1):
+        print(board[row])
+
+def PlayerMove(place):
+    place-=1
+    for row in range(6):
+        if board[row][place]=="0":
+            board[row][place] = "X"
+            return True
+    return print("Chcesz wyjść za mape!") #trzeba dać użytkonikowi ruch jak się stara wyjść za mape
+
+def BotMove(place):
+    for row in range(6):
+        if board[row][place]=="0":
+            board[row][place] = "O"
+            return True
+    return False
+
+
+#Główny program
+BoardPrint()
+while(1):
+    place=int(input("Gdzie chcesz postawić X?(1-7): "))
+    PlayerMove(place)
+    BotMove(random.randrange(7))
+    BoardPrint()
