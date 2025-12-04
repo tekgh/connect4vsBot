28883ed74fbaa8909cdfe8913e46e7b9267cea98
tek <126421917+tekgh@users.noreply.github.com>
2025-12-04 19:41:38 +0100

    Refactor bot move logic and improve player input handling
    Made code a bit easier to read and overall optimised it's length.

--- connect4.py
+++ connect4.py
@@ -1,55 +1,45 @@
 import random
+
 board = [["0" for _ in range(7)] for _ in range(6)]
 def BoardPrint():
     for row in range(5, -1, -1):
         print(board[row])
 
 def PlayerMove(place):
-    place-=1
     for row in range(6):
         if board[row][place]=="0":
             board[row][place] = "X"
             return True
     return False
 
-def BotMove(place,playerplace):
-    playerplace-=1
+def BotMove(RandomPlace,playerplace):
     i = random.randrange(4)
-    if i == 0: #losowy ruch
-        for row in range(6):
-            if board[row][place]=="0":
-                board[row][place] = "O"
-                return True
-    if i == 1: #przesuniecie jeden w lewo jesli możliwe
+
+    #losowy ruch
+    if i == 0: 
+        move = RandomPlace
+
+    #przesuniecie jeden w lewo jesli możliwe
+    if i == 1:
         if playerplace ==0:
-            for row in range(6):
-                if board[row][0]=="0":
-                    board[row][0] = "O"
-                    return True
+            move = 0
         else:
-            playerplace-=1
-            for row in range(6):
-                if board[row][playerplace]=="0":
-                    board[row][playerplace] = "O"
-                    return True
-    if i ==2: #przesuniecie jeden w prawo jesli możliwe
+            move = playerplace-1
+
+    #przesuniecie jeden w prawo jesli możliwe
+    if i ==2:
         if playerplace ==6:
-            for row in range(6):
-                if board[row][6]=="0":
-                    board[row][6] = "O"
-                    return True
+            move = 6
         else:
-            playerplace+=1
-            for row in range(6):
-                if board[row][playerplace]=="0":
-                    board[row][playerplace] = "O"
-                    return True
-    if i == 3: #postawienie X tam gdzie gracz postawił
-        for row in range(6):
-            if board[row][playerplace]=="0":
-                board[row][playerplace] = "O"
+            move = playerplace+1
+
+    #postawienie X tam gdzie gracz postawił
+    if i == 3:
+        move = playerplace
+    for row in range(6):
+            if board[row][move]=="0":
+                board[row][move] = "O"       
                 return True
-            
     return False
 
 def WinCon(player):
@@ -58,16 +48,19 @@ def WinCon(player):
         for col in range(4):
             if all(board[row][col+i] ==player for i in range(4)):
                 return True
+            
     #pion
     for row in range (3):
         for col in range(7):
             if all(board[row+i][col] == player for i in range(4)):
                 return True
+            
     #skos/
     for row in range (4):
         for col in range(3):
             if all(board[row+i][col+i] == player for i in range(4)):
                 return True
+            
     #skos\
     for row in range (3):
         for col in range(3,7):
@@ -75,15 +68,14 @@ def WinCon(player):
                 return True
     return False
     
-     
 #Główny program
 BoardPrint()
 while(1):
-    
+    #ruch gracza
     while True:
         try:
-            place = int(input("Gdzie chcesz postawić X? (1-7): "))
-            if place < 1 or place > 7:
+            place = int(input("Gdzie chcesz postawić X? (1-7): "))-1
+            if place < 0 or place > 6:
                 print("Podaj liczbę od 1 do 7.")
                 continue
             if not PlayerMove(place):
@@ -92,17 +84,19 @@ while(1):
             break
         except ValueError:
             print("To nie jest liczba! Spróbuj ponownie.")
-        
+    #sprawdzenie czy gracz wygrał
     if WinCon("X"):
         BoardPrint()
         print("Wygrałeś!")
         break
-
+    
+    #ruch bota
     while True:
         bot_choice = random.randrange(7)
         if BotMove(bot_choice, place):
             break
-    
+        
+    #sprawdzenie czy bot wygrał
     if WinCon("O"):
         BoardPrint()
         print("Przegrałeś!")
